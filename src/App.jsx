fd40b4dcc824d7908e37dc1abc8ef1ad19a62737
Andy Thawng <thawngandy@gmail.com>
2025-10-02 03:32:00 +0630

    First Api Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,93 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Search from './components/Search.jsx'
+import Spinner from './components/Spinner.jsx'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    'accept': 'application/json',
+    'Authorization': `Bearer ${API_KEY}`
+  }
+}
+
 const App = () => {
 
   const [query, setQuery] = useState('');
+  const [isLoading, setisLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [movies, setMovies] = useState([])
+
+  const fetchMovies = async () => {
+
+    setisLoading(true);
+    setError(null);
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS);
+      const data = await response.json();
+      
+      if (!response.ok) {
+        throw new Error(data.status_message || 'Failed to fetch movies.');
+      }
+
+      if (!data.results || data.results.length === 0) {
+        setMovies([]);
+        setError('No movies found.');
+        return;
+      }
+
+      setMovies(data.results);
+      setError(null);
+
+
+    } catch (error) {
+      console.log(error);
+      setError('Failed to fetch movies. Please try again later.');
+    } finally {
+      setisLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, [])
+
   return (
     <main>
       <div className='pattern'/>
-      <div className='wrapper'>
-        <header>
-          <img src="/hero-img.png" alt="Hero Banner" />
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-        </header>
+        <div className='wrapper'>
+          <header>
+            <img src="/hero-img.png" alt="Hero Banner" />
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
 
-        <Search query={query} setQuery={setQuery}/>
+            <Search query={query} setQuery={setQuery}/>
+          </header>
 
-        <h1>{query}</h1>
-      </div>
+          <section className='all-movies'>
+            <h2 className='mt-[40px]'>All Movies</h2>
+
+            {isLoading ? (
+               <Spinner />
+            ) : error ? (
+              <p className='text-red-500'>{error}</p>
+            ) : (
+              <ul>
+                {movies.map((movie) => (
+                  <p className='text-white' key={movie.id}>{movie.title}</p>
+                ))}
+              </ul>
+            )}
+          </section>
+
+
+
+          <h1>{query}</h1>
+        </div>
     </main>
   )
 }
